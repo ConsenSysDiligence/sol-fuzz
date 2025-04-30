2d60c912760f417034718874fa53cfdf9418b482
Dimitar Bounov <dimitar.bounov@consensys.net>
2025-04-30 10:07:21 -1000

    nit: re-export Rewrite for use in solc-fuzz

--- src/rewrites/index.ts
+++ src/rewrites/index.ts
@@ -1,4 +1,4 @@
-export { BaseRule, GenEnv, GenRule, RewriteRule, makeRewrite, parseRules } from "./dsl";
+export { BaseRule, GenEnv, GenRule, makeRewrite, parseRules, Rewrite, RewriteRule } from "./dsl";
 export { PeggySyntaxError as SyntaxError } from "./dsl/parser_gen";
 export * from "./rewrite";
 export * from "./template";
